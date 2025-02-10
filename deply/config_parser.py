70b3395990bb8901b6fe5e6871a4870c29187b2e
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 14:03:13 +0000

    Reset to f22459acd7dbb462ef1086b2b9d46c48a830936e and made changes for LLM Round 1

--- deply/config_parser.py
+++ deply/config_parser.py
@@ -13,11 +13,11 @@ class ConfigParser:
             config = yaml.safe_load(f)
 
         config = config.get('deply', config)
-        if 'paths' not in config:
-            config['paths'] = [str(self.config_path.parent)]
         config.setdefault('exclude_files', [])
         config.setdefault('layers', [])
         config.setdefault('ruleset', {})
+        if 'paths' not in config or not config['paths']:
+            config['paths'] = [str(self.config_path.parent)]
 
         return config
 
