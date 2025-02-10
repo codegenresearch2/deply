1ce6bb7abf42400424af8a89e059f7a9e06e9d79
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 13:16:56 +0000

    Reset to 20c2d6b4f57df462b161780df897355e12debeea and made changes for LLM Round 1

--- deply/collectors/base_collector.py
+++ deply/collectors/base_collector.py
@@ -1,11 +1,9 @@
-import ast
 from abc import ABC, abstractmethod
-from pathlib import Path
-
+from typing import Set
 from ..models.code_element import CodeElement
 
 
 class BaseCollector(ABC):
     @abstractmethod
-    def match_in_file(self, file_ast: ast.AST, file_path: Path) -> set[CodeElement]:
+    def collect(self) -> Set[CodeElement]:
         pass
