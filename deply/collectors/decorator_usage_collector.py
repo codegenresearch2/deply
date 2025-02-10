c70e9ac1f7f993fc4a3c368545fa318db62f6027
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 14:41:07 +0000

    Reset to 20c2d6b4f57df462b161780df897355e12debeea and made changes for LLM Round 1

--- deply/collectors/decorator_usage_collector.py
+++ deply/collectors/decorator_usage_collector.py
@@ -57,7 +57,7 @@ class DecoratorUsageCollector(BaseCollector):
             if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef)):
                 for decorator in node.decorator_list:
                     d_name = self._get_decorator_name(decorator)
-                    if self.decorator_regex and self.decorator_regex.match(d_name):
+                    if self.decorator_name == d_name or (self.decorator_regex and self.decorator_regex.match(d_name)):
                         full_name = self._get_full_name(node)
                         code_element = CodeElement(
                             file=file_path,
