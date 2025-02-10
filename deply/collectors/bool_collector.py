b9d67d0810554afad3e3c95c444b242eca00fa2d
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 15:04:53 +0000

    Reset to 20c2d6b4f57df462b161780df897355e12debeea and made changes for LLM Round 1

--- deply/collectors/bool_collector.py
+++ deply/collectors/bool_collector.py
@@ -4,45 +4,20 @@ import ast
 
 from deply.models.code_element import CodeElement
 from .base_collector import BaseCollector
+from .collector_factory import CollectorFactory
 
-class FlexibleBoolCollector(BaseCollector):
+class BoolCollector(BaseCollector):
     def __init__(self, config: Dict[str, Any], paths: List[str], exclude_files: List[str]):
         self.paths = paths
         self.exclude_files = exclude_files
-        self.must_configs = config.get('must', [])
-        self.any_of_configs = config.get('any_of', [])
-        self.must_not_configs = config.get('must_not', [])
+        self.must_collectors = [CollectorFactory.create(collector_config, paths, exclude_files) for collector_config in config.get('must', [])]
+        self.any_of_collectors = [CollectorFactory.create(collector_config, paths, exclude_files) for collector_config in config.get('any_of', [])]
+        self.must_not_collectors = [CollectorFactory.create(collector_config, paths, exclude_files) for collector_config in config.get('must_not', [])]
 
     def collect(self) -> Set[CodeElement]:
-        from .collector_factory import CollectorFactory
-
-        must_sets = []
-        for collector_config in self.must_configs:
-            collector = CollectorFactory.create(collector_config, self.paths, self.exclude_files)
-            elements = collector.collect()
-            must_sets.append(elements)
-
-        any_of_sets = []
-        for collector_config in self.any_of_configs:
-            collector = CollectorFactory.create(collector_config, self.paths, self.exclude_files)
-            elements = collector.collect()
-            any_of_sets.append(elements)
-
-        must_not_elements = set()
-        for collector_config in self.must_not_configs:
-            collector = CollectorFactory.create(collector_config, self.paths, self.exclude_files)
-            elements = collector.collect()
-            must_not_elements.update(elements)
-
-        if must_sets:
-            must_elements = set.intersection(*must_sets)
-        else:
-            must_elements = set()
-
-        if any_of_sets:
-            any_of_elements = set.union(*any_of_sets)
-        else:
-            any_of_elements = set()
+        must_elements = set.intersection(*[collector.collect() for collector in self.must_collectors]) if self.must_collectors else set()
+        any_of_elements = set.union(*[collector.collect() for collector in self.any_of_collectors]) if self.any_of_collectors else set()
+        must_not_elements = set.union(*[collector.collect() for collector in self.must_not_collectors]) if self.must_not_collectors else set()
 
         if must_elements and any_of_elements:
             combined_elements = must_elements & any_of_elements
@@ -58,11 +33,8 @@ class FlexibleBoolCollector(BaseCollector):
         return final_elements
 
     def match_in_file(self, file_ast: ast.AST, file_path: Path) -> Set[CodeElement]:
-        from .collector_factory import CollectorFactory
-
         elements = set()
-        for collector_config in self.must_configs + self.any_of_configs + self.must_not_configs:
-            collector = CollectorFactory.create(collector_config, [str(file_path)], self.exclude_files)
+        for collector in self.must_collectors + self.any_of_collectors + self.must_not_collectors:
             elements.update(collector.match_in_file(file_ast, file_path))
 
         return elements
