5f6136fe10cac6db279fe9836ee39924fc0c2a21
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 15:48:45 +0000

    Reset to f22459acd7dbb462ef1086b2b9d46c48a830936e and made changes for LLM Round 1

--- tests/test_collectors.py
+++ tests/test_collectors.py
@@ -64,31 +64,44 @@ class TestCollectors(unittest.TestCase):
         utils_py.write_text('@utility_decorator\ndef helper_function():\n    pass\n')
 
     def test_class_inherits_collector(self):
-        collector_config = {'base_class': 'BaseModel'}
-        self.assert_collector(ClassInheritsCollector, collector_config, {'UserModel'})
+        collector = ClassInheritsCollector({'base_class': 'BaseModel'}, [str(self.test_project_dir)], [])
+        collected_elements = collector.collect()
+        collected_class_names = {element.name for element in collected_elements}
+        self.assertEqual(collected_class_names, {'UserModel'})
 
     def test_file_regex_collector(self):
-        collector_config = {'regex': r'.*controller.py$'}
-        self.assert_collector(FileRegexCollector, collector_config, {'BaseController', 'UserController'})
+        collector = FileRegexCollector({'regex': r'.*controller.py$'}, [str(self.test_project_dir)], [])
+        collected_elements = collector.collect()
+        collected_class_names = {element.name for element in collected_elements}
+        self.assertEqual(collected_class_names, {'BaseController', 'UserController'})
 
     def test_class_name_regex_collector(self):
-        collector_config = {'class_name_regex': '^User.*'}
-        self.assert_collector(ClassNameRegexCollector, collector_config, {'UserController', 'UserModel', 'UserService'})
+        collector = ClassNameRegexCollector({'class_name_regex': '^User.*'}, [str(self.test_project_dir)], [])
+        collected_elements = collector.collect()
+        collected_class_names = {element.name for element in collected_elements}
+        self.assertEqual(collected_class_names, {'UserController', 'UserModel', 'UserService'})
 
     def test_directory_collector(self):
-        collector_config = {'directories': ['services']}
-        self.assert_collector(DirectoryCollector, collector_config, {'BaseService', 'UserService'})
+        collector = DirectoryCollector({'directories': ['services']}, [str(self.test_project_dir)], [])
+        collected_elements = collector.collect()
+        collected_class_names = {element.name for element in collected_elements}
+        self.assertEqual(collected_class_names, {'BaseService', 'UserService'})
 
     def test_decorator_usage_collector(self):
-        collector_config = {'decorator_name': 'login_required'}
-        self.assert_collector(DecoratorUsageCollector, collector_config, {'UserController'})
+        collector = DecoratorUsageCollector({'decorator_name': 'login_required'}, [str(self.test_project_dir)], [])
+        collected_elements = collector.collect()
+        collected_names = {element.name for element in collected_elements}
+        self.assertEqual(collected_names, {'UserController'})
 
-        collector_config = {'decorator_regex': '^.*decorator$'}
-        self.assert_collector(DecoratorUsageCollector, collector_config, {'UserService', 'helper_function'})
+        collector = DecoratorUsageCollector({'decorator_regex': '^.*decorator$'}, [str(self.test_project_dir)], [])
+        collected_elements = collector.collect()
+        collected_names = {element.name for element in collected_elements}
+        self.assertEqual(collected_names, {'UserService', 'helper_function'})
 
     def test_class_name_regex_collector_no_matches(self):
-        collector_config = {'class_name_regex': '^NonExistentClass.*'}
-        self.assert_collector(ClassNameRegexCollector, collector_config, set())
+        collector = ClassNameRegexCollector({'class_name_regex': '^NonExistentClass.*'}, [str(self.test_project_dir)], [])
+        collected_elements = collector.collect()
+        self.assertEqual(len(collected_elements), 0)
 
     def test_bool_collector(self):
         collector_config = {
@@ -100,7 +113,10 @@ class TestCollectors(unittest.TestCase):
                 {'type': 'decorator_usage', 'decorator_name': 'deprecated_service'}
             ]
         }
-        self.assert_collector(BoolCollector, collector_config, {'UserService'})
+        collector = BoolCollector(collector_config, [str(self.test_project_dir)], [])
+        collected_elements = collector.collect()
+        collected_class_names = {element.name for element in collected_elements}
+        self.assertEqual(collected_class_names, {'UserService'})
 
     def test_directory_collector_with_rules(self):
         user_controller_py = self.test_project_dir / 'controllers' / 'user_controller.py'
@@ -125,19 +141,13 @@ class TestCollectors(unittest.TestCase):
             yaml.dump(config_data, f)
         with self.capture_output() as (out, err):
             try:
-                sys.argv = ['main.py', '--config', str(config_yaml)]
+                sys.argv = ['main.py', 'analyze', '--config', str(config_yaml)]
                 main()
             except SystemExit as e:
                 exit_code = e.code
         output = out.getvalue()
         self.assertEqual(exit_code, 1)
         self.assertIn("Layer 'controllers_layer' is not allowed to depend on layer 'models_layer'", output)
-
-    def assert_collector(self, collector_class, collector_config, expected_classes):
-        collector = collector_class(collector_config, [str(self.test_project_dir)], [])
-        collected_elements = collector.collect()
-        collected_class_names = {element.name for element in collected_elements}
-        self.assertEqual(collected_class_names, expected_classes)
 
     @staticmethod
     def capture_output():
