33b496047fb29a2493524acaba005684e8e00c67
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 15:48:45 +0000

    Reset to 20c2d6b4f57df462b161780df897355e12debeea and made changes for LLM Round 1

--- deply/collectors/bool_collector.py
+++ deply/collectors/bool_collector.py
@@ -4,63 +4,78 @@ import ast
 
 from deply.models.code_element import CodeElement
 from .base_collector import BaseCollector
+from .collector_factory import CollectorFactory
 
 class BoolCollector(BaseCollector):
     def __init__(self, config: Dict[str, Any], paths: List[str], exclude_files: List[str]):
         self.paths = paths
         self.exclude_files = exclude_files
-        self.must_configs = config.get('must', [])
-        self.any_of_configs = config.get('any_of', [])
-        self.must_not_configs = config.get('must_not', [])
-        self.must_collectors = []
-        self.any_of_collectors = []
-        self.must_not_collectors = []
-        self.initialize_collectors()
-
-    def initialize_collectors(self):
-        from .collector_factory import CollectorFactory
-        self.must_collectors = [CollectorFactory.create(collector_config, self.paths, self.exclude_files) for collector_config in self.must_configs]
-        self.any_of_collectors = [CollectorFactory.create(collector_config, self.paths, self.exclude_files) for collector_config in self.any_of_configs]
-        self.must_not_collectors = [CollectorFactory.create(collector_config, self.paths, self.exclude_files) for collector_config in self.must_not_configs]
+        self.must_collectors = [CollectorFactory.create(collector_config, paths, exclude_files) for collector_config in config.get('must', [])]
+        self.any_of_collectors = [CollectorFactory.create(collector_config, paths, exclude_files) for collector_config in config.get('any_of', [])]
+        self.must_not_collectors = [CollectorFactory.create(collector_config, paths, exclude_files) for collector_config in config.get('must_not', [])]
 
     def collect(self) -> Set[CodeElement]:
-        must_sets = [collector.collect() for collector in self.must_collectors]
-        any_of_sets = [collector.collect() for collector in self.any_of_collectors]
-        must_not_elements = set.union(*[collector.collect() for collector in self.must_not_collectors]) if self.must_not_collectors else set()
+        must_sets = []
+        for collector in self.must_collectors:
+            elements = collector.collect()
+            if elements:
+                must_sets.append(elements)
 
-        must_elements = set.intersection(*must_sets) if must_sets else None
-        any_of_elements = set.union(*any_of_sets) if any_of_sets else None
+        any_of_sets = []
+        for collector in self.any_of_collectors:
+            elements = collector.collect()
+            if elements:
+                any_of_sets.append(elements)
 
-        if must_elements is not None and any_of_elements is not None:
-            combined_elements = must_elements & any_of_elements
-        elif must_elements is not None:
-            combined_elements = must_elements
-        elif any_of_elements is not None:
-            combined_elements = any_of_elements
+        must_not_elements = set()
+        for collector in self.must_not_collectors:
+            elements = collector.collect()
+            must_not_elements.update(elements)
+
+        if must_sets:
+            must_elements = set.intersection(*must_sets)
         else:
-            combined_elements = set()
+            must_elements = set()
 
+        if any_of_sets:
+            any_of_elements = set.union(*any_of_sets)
+        else:
+            any_of_elements = set()
+
+        combined_elements = must_elements & any_of_elements
         final_elements = combined_elements - must_not_elements
 
         return final_elements
 
     def match_in_file(self, file_ast: ast.AST, file_path: Path) -> Set[CodeElement]:
-        must_sets = [collector.match_in_file(file_ast, file_path) for collector in self.must_collectors]
-        any_of_sets = [collector.match_in_file(file_ast, file_path) for collector in self.any_of_collectors]
-        must_not_elements = set.union(*[collector.match_in_file(file_ast, file_path) for collector in self.must_not_collectors]) if self.must_not_collectors else set()
+        must_sets = []
+        for collector in self.must_collectors:
+            elements = collector.match_in_file(file_ast, file_path)
+            if elements:
+                must_sets.append(elements)
 
-        must_elements = set.intersection(*must_sets) if must_sets else None
-        any_of_elements = set.union(*any_of_sets) if any_of_sets else None
+        any_of_sets = []
+        for collector in self.any_of_collectors:
+            elements = collector.match_in_file(file_ast, file_path)
+            if elements:
+                any_of_sets.append(elements)
 
-        if must_elements is not None and any_of_elements is not None:
-            combined_elements = must_elements & any_of_elements
-        elif must_elements is not None:
-            combined_elements = must_elements
-        elif any_of_elements is not None:
-            combined_elements = any_of_elements
+        must_not_elements = set()
+        for collector in self.must_not_collectors:
+            elements = collector.match_in_file(file_ast, file_path)
+            must_not_elements.update(elements)
+
+        if must_sets:
+            must_elements = set.intersection(*must_sets)
         else:
-            combined_elements = set()
+            must_elements = set()
 
+        if any_of_sets:
+            any_of_elements = set.union(*any_of_sets)
+        else:
+            any_of_elements = set()
+
+        combined_elements = must_elements & any_of_elements
         final_elements = combined_elements - must_not_elements
 
         return final_elements
