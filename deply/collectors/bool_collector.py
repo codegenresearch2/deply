ebe1f842c8d95eddc0708c224e93350080a32aa5
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 14:53:43 +0000

    Reset to 20c2d6b4f57df462b161780df897355e12debeea and made changes for LLM Round 1

--- deply/collectors/bool_collector.py
+++ deply/collectors/bool_collector.py
@@ -11,6 +11,11 @@ class BoolCollector(BaseCollector):
         self.must_configs = config.get('must', [])
         self.any_of_configs = config.get('any_of', [])
         self.must_not_configs = config.get('must_not', [])
+        
+        # Pre-instantiate sub-collectors
+        self.must_collectors = [CollectorFactory.create(cfg, paths, exclude_files) for cfg in self.must_configs]
+        self.any_of_collectors = [CollectorFactory.create(cfg, paths, exclude_files) for cfg in self.any_of_configs]
+        self.must_not_collectors = [CollectorFactory.create(cfg, paths, exclude_files) for cfg in self.must_not_configs]
 
     def match_in_file(self, file_ast: ast.AST, file_path: Path) -> Set[CodeElement]:
         elements = set()
@@ -20,23 +25,45 @@ class BoolCollector(BaseCollector):
     def collect(self) -> Set[CodeElement]:
         from .collector_factory import CollectorFactory
 
-        must_elements = set()
-        any_of_elements = set()
+        must_elements_list = []
+        any_of_elements_list = []
         must_not_elements = set()
 
-        for collector_config in self.must_configs:
-            collector = CollectorFactory.create(collector_config, self.paths, self.exclude_files)
-            must_elements.update(collector.collect())
+        # Collect elements from must collectors
+        for collector in self.must_collectors:
+            must_elements_list.append(collector.collect())
 
-        for collector_config in self.any_of_configs:
-            collector = CollectorFactory.create(collector_config, self.paths, self.exclude_files)
-            any_of_elements.update(collector.collect())
+        # Collect elements from any_of collectors
+        for collector in self.any_of_collectors:
+            any_of_elements_list.append(collector.collect())
 
-        for collector_config in self.must_not_configs:
-            collector = CollectorFactory.create(collector_config, self.paths, self.exclude_files)
+        # Collect elements from must_not collectors
+        for collector in self.must_not_collectors:
             must_not_elements.update(collector.collect())
 
-        combined_elements = must_elements & any_of_elements
+        # Combine must elements
+        if must_elements_list:
+            must_elements = set.intersection(*must_elements_list)
+        else:
+            must_elements = None
+
+        # Combine any_of elements
+        if any_of_elements_list:
+            any_of_elements = set.union(*any_of_elements_list)
+        else:
+            any_of_elements = None
+
+        # Combine must and any_of elements
+        if must_elements is not None and any_of_elements is not None:
+            combined_elements = must_elements & any_of_elements
+        elif must_elements is not None:
+            combined_elements = must_elements
+        elif any_of_elements is not None:
+            combined_elements = any_of_elements
+        else:
+            combined_elements = set()
+
+        # Subtract must_not elements
         final_elements = combined_elements - must_not_elements
 
         return final_elements
